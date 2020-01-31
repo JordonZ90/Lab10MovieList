690435f197d092a99d16b000f485de04bbcb03bf
Jordon <jzenterprize90@gmail.com>
2020-01-31 06:47:46 -0500

    First push of Movie Lists Lab 10

---
+++ Lab10MovieList/Movie.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab10MovieList
+{
+    class Movie
+    {
+        //fields
+        private string name;
+        private string category;
+
+        //properties
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Category
+        {
+            get { return category; }
+            set { category = value; }
+        }
+
+        //constructors
+        public Movie() { }
+
+        public Movie(string _name, string _category)
+        {
+            name = _name;
+            category = _category;
+        }
+
+        //methods
+
+        private static List<string> GetListOfGenreType(List<Movie> genreType)//Get the list of categories 
+        {
+            List<string> genre = new List<string>();
+            foreach (Movie name in genreType)
+            {
+                if (!genre.Contains(name.category))
+                {
+                    genre.Add(name.category);
+                }
+            }
+            return genre;
+        }
+        public static void PrintOutGenreTypes(List<Movie> movieType)
+        {
+            //Printing out the genres
+            List<string> genreType = GetListOfGenreType(movieType);
+
+            for (int i = 0; i < genreType.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {genreType[i]}");
+            }
+            Console.WriteLine();
+
+        }
+        public static List<string> GetListOfMovies(List<Movie> moviesCategory, string selection)
+        {
+            //Getting the list of movies
+            List<string> moviesList = new List<string>();
+
+            foreach (Movie film in moviesCategory)
+            {
+                if (film.category == selection)
+                {
+                    moviesList.Add(film.name);
+                }
+            }
+            return moviesList;
+        }
+
+        public static void PrintOutFims(List<Movie> filmList, string choice)
+        {
+            // Printing out the movies
+            List<string> printFilms = GetListOfMovies(filmList, choice);
+            printFilms.Sort();
+            Console.WriteLine($"The list of {choice} movies are: ");
+            for (int i = 0; i < printFilms.Count; i++)
+            {
+                Console.WriteLine($"{printFilms[i]}");
+            }
+
+        }
+
+    }
+}
---
+++ Lab10MovieList/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab10MovieList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   //greeting user
+            Console.WriteLine("Welcome to MovieLand!");
+            //created list of class Moive to store the data for title and category per movie
+            List<Movie> filmList = new List<Movie>
+            {
+                new Movie("Titannic", "drama"),
+                new Movie("Predator", "scifi"),
+                new Movie("Apollo 11", "drama"),
+                new Movie("The Notebook", "drama"),
+                new Movie("Saw", "horror"),
+                new Movie("Forest Gump", "drama"),
+                new Movie("Saving Private Ryan", "horror"),
+                new Movie("Get Out", "drama"),
+                new Movie("Blade Runner", "scifi"),
+                new Movie("Star Wars Christmas Special", "horror"),
+
+            };
+
+            bool userContinue = true;
+            while (userContinue)
+            {
+                Console.WriteLine("Here are the genres: ");
+
+                //printing the list if genres                
+                Movie.PrintOutGenreTypes(filmList);
+
+                string genreChoice = selectionCategory("Please choose pick a number for the desired genre");
+                Movie.PrintOutFims(filmList, genreChoice);
+                userContinue = UserSelection("Do you want to continue? please (y/n)", "y", "n");
+            }
+            Console.WriteLine("Thank you and don't forget to return your movie or do we do not care!");
+        }
+
+
+
+        public static string GetUserInput(string message)
+        {
+            Console.WriteLine(message);
+            return Console.ReadLine();
+        }
+
+        public static bool UserSelection(string message, string trueChoice, string falseChoice)
+        {
+            string select = GetUserInput(message);
+
+            if (select == trueChoice)
+            {
+                return true;
+            }
+            else if (select == falseChoice)
+            {
+                return false;
+            }
+            else
+            {
+                return UserSelection("Your entry is incorrect! " + message, trueChoice, falseChoice);
+            }
+        }
+
+        public static string selectionCategory(string message)
+        {
+            string input = GetUserInput(message);
+            if (input == "")
+            {
+                return selectionCategory("Nothing is nothing please enter something " + message);
+            }
+            else
+            {
+                int number = int.Parse(input);
+                Console.WriteLine();
+
+                if (number - 1 == 0)
+                {
+                    return "drama";
+                }
+                else if (number - 1 == 1)
+                {
+                    return "scifi";
+                }
+                else if (number - 1 == 2)
+                {
+                    return "horror";
+                }
+
+                else
+                {
+                    return selectionCategory("Invalid Entry! " + message);
+                }
+            }
+        }
+    }
+}
